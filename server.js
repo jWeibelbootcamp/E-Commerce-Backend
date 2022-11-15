6bc6d9bb5ec65e80e6e086bea792fde9ccc526a4
devWeibel <weibel.jason@gmail.com>
2022-11-14 20:03:44 -0500

    models index.js and import to server.js;

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const Model = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
