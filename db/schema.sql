154233fe3262cb3f9d23c196cf39e50ac26ef22e
devWeibel <weibel.jason@gmail.com>
2022-11-17 23:42:21 -0500

    trying to fix sequelize connection error

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,3 @@
--- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
-
--- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
-
+USE ecommerce_db;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-const Model = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-sequelize.sync().then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
