3beaeec2032f42388bb5cc8ec9a0dfefe4e893a7
devWeibel <weibel.jason@gmail.com>
2022-11-18 09:15:00 -0500

    working on sequelize bug still

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const sequelize = require('./config/connection.js');
 const routes = require('./routes');
-const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
